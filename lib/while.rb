e7fa181aee08ba5bdb65540fd4d91bdaa71b5f9d
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-09 18:21:43 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,8 @@
 def using_while
     levitation_force = 6
+    levitation_force = levitation_force +1
     while levitation_force < 10
-    puts "Wingardium Leviousa"
+    puts "Wingardium Leviosa"
     levitation_force += 1
  end
 	
